8bd5fc66765a837941ad2ba43b3b1cf6137b9b0d
Talha Karasu <talhafb1223@gmail.com>
2021-01-09 02:15:27 +0300

    add sample data to reportMeeting

--- src/store/modules/reportMeeting.js
+++ src/store/modules/reportMeeting.js
@@ -1,7 +1,68 @@
 import axios from "axios"
 
 const state = {
-  reportMeeting: [],
+  reportMeeting: [
+    {
+      id: "ZHuwL745TyaVmJ5PsiFsIQ",
+      user_name: "Talha Karasu",
+      user_number: 136,
+      user_class: "9/A",
+      report_time: [
+        {
+          join_time: "a2021-01-06T15:37:18Z",
+          leave_time: "2021-01-06T15:37:18Z",
+        },
+        {
+          join_time: "b2021-01-06T15:37:18Z",
+          leave_time: "2021-01-06T15:37:18Z",
+        },
+        {
+          join_time: "c2021-01-06T15:37:18Z",
+          leave_time: "2021-01-06T15:37:18Z",
+        },
+      ],
+    },
+    {
+      id: "Bkjgasd7GVBDi7ajl8H6qI",
+      user_name: "Faruk Olusan",
+      user_number: 358,
+      user_class: "9/B",
+      report_time: [
+        {
+          join_time: "d2021-01-06T15:37:18Z",
+          leave_time: "2021-01-06T15:37:18Z",
+        },
+        {
+          join_time: "e2021-01-06T15:37:18Z",
+          leave_time: "2021-01-06T15:37:18Z",
+        },
+        {
+          join_time: "f2021-01-06T15:37:18Z",
+          leave_time: "2021-01-06T15:37:18Z",
+        },
+      ],
+    },
+    {
+      id: "Nhasdjn8Oaso8AHShndO8Q",
+      user_name: "Bora Cetkin",
+      user_number: 756,
+      user_class: "9/C",
+      report_time: [
+        {
+          join_time: "g2021-01-06T15:37:18Z",
+          leave_time: "2021-01-06T15:37:18Z",
+        },
+        {
+          join_time: "h2021-01-06T15:37:18Z",
+          leave_time: "2021-01-06T15:37:18Z",
+        },
+        {
+          join_time: "j2021-01-06T15:37:18Z",
+          leave_time: "2021-01-06T15:37:18Z",
+        },
+      ],
+    },
+  ],
 }
 
 const getters = {
