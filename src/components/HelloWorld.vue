353c74d114c17cce1736701a592c9c35b1b0b818
Talha Karasu <talhafb1223@gmail.com>
2021-01-09 02:20:26 +0300

    frontend

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <v-row>
       <v-col cols="12">
         <v-card
-          style="line-height: 33px; background-color: pink;"
+          style="line-height: 33px;"
           class="text-center py-3"
           elevation="24"
           shaped
@@ -16,11 +16,8 @@
               <v-col cols="3">
                 DURATION
               </v-col>
-              <v-col cols="4">
+              <v-col cols="6">
                 TOPIC
-              </v-col>
-              <v-col cols="2">
-                HOST
               </v-col>
               <v-spacer />
             </v-row>
@@ -30,17 +27,19 @@
                 LIVE
               </v-col>
               <v-col cols="3" class="green--text text--darken-2">
-                for 27 Minute and 35 Seconds
+                <span v-if="status == 'LIVE'">
+                  {{ new Date().getTime() - getReportMeeting.join_time }}
+                </span>
+                <span v-if="status == 'ENDED'">
+                  {{ getReportMeeting.duration }}
+                </span>
               </v-col>
               <v-col>
-                Bora Cetkin's meeting
-              </v-col>
-              <v-col cols="2">
-                Bora Cetkin
+                {{ getReportMeeting.topic }}
               </v-col>
               <v-col cols="2">
                 <v-btn>
-                  details
+                  DETAILS
                 </v-btn>
               </v-col>
             </v-row>
@@ -52,21 +51,58 @@
     <v-row>
       <v-col cols="12">
         <v-card>
-          <v-card-title style="background-color: pink;">
+          <v-card-title>
             <v-text-field
               v-model="search"
               append-icon="mdi-magnify"
               label="Search"
-              single-line
               hide-details
             ></v-text-field>
           </v-card-title>
           <v-data-table
-            style="background-color: pink;"
-            :headers="createHeaders(allParticipants[0])"
-            :items="allParticipants"
+            :headers="
+              status == 'LIVE'
+                ? createLiveHeaders(getParticipants[0])
+                : createEndedHeaders(getReportPolling.members[0])
+            "
+            :items="getParticipants"
             :search="search"
-          ></v-data-table>
+            :single-expand="singleExpand"
+            :expanded.sync="expanded"
+            item-key="user_number"
+            show-expand
+          >
+            <template v-slot:top>
+              <v-toolbar flat>
+                <v-toolbar-title>Participants</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-switch
+                  v-model="singleExpand"
+                  label="Single expand"
+                  class="mt-2"
+                ></v-switch>
+              </v-toolbar>
+            </template>
+            <template v-slot:expanded-item="{ headers, item }">
+              <td :colspan="headers.length">
+                <span
+                  class="my-10 d-block"
+                  outlined
+                  :v-for="
+                    (report, index) in status == 'LIVE'
+                      ? findParticipant(item).report_time
+                      : item.report_time
+                  "
+                  :key="status == 'LIVE' ? index : item.id"
+                >
+                  Joined at : {{ report.join_time }}
+                  <br />
+                  Left at : {{ report.leave_time }}
+                  <v-divider />
+                </span>
+              </td>
+            </template>
+          </v-data-table>
         </v-card>
       </v-col>
     </v-row>
@@ -81,26 +117,55 @@ export default {
 
   data: () => ({
     search: "",
+    singleExpand: true,
+    expanded: [],
   }),
   methods: {
-    createHeaders(obj) {
+    createLiveHeaders(obj) {
       obj = Object.keys(obj)
-      obj.shift()
       return [
         {
           text: "Name",
           align: "start",
-          filterable: false,
-          value: obj[0],
+          value: obj[1],
         },
-        { text: "Class", value: obj[1] },
         { text: "Number", value: obj[2] },
-        { text: "Join Time", value: obj[3] },
+        { text: "Class", value: obj[3] },
+        { text: "Join Time", value: obj[4] },
+        { text: "Actions", value: "data-table-expand" },
       ]
+    },
+    createEndedHeaders(obj) {
+      obj = Object.keys(obj)
+      return [
+        {
+          text: "Name",
+          align: "start",
+          value: obj[1],
+        },
+        { text: "Number", value: obj[2] },
+        { text: "Class", value: obj[3] },
+        { text: "Duration", value: obj[5] },
+        { text: "Here", value: obj[6] },
+        { text: "Actions", value: "data-table-expand" },
+      ]
+    },
+    findParticipant(p) {
+      return this.getReportsPerParticipant.find(
+        (participant) => participant.id == p.id
+      )
+    },
+    log(d) {
+      console.log(d)
     },
   },
   computed: {
-    ...mapGetters(["allParticipants"]),
+    ...mapGetters([
+      "getParticipants",
+      "getReportMeeting",
+      "getReportsPerParticipant",
+      "getReportPolling",
+    ]),
   },
 }
 </script>
