592d72b4644a59ab8735ba6f099e5d44103d7ae8
Talha Karasu <talhafb1223@gmail.com>
2021-01-11 23:48:39 +0300

    fix for mobile devices

--- src/components/Meeting.vue
+++ src/components/Meeting.vue
@@ -3,18 +3,19 @@
     <v-row style="line-height: 35px;" class="text-center">
       <v-col
         v-if="!meeting.end_time"
-        cols="1"
+        sm="1"
+        xs="12"
         class="green--text text--darken-2 text-h6 font-weight-bold"
       >
         {{ "LIVE" }}
       </v-col>
-      <v-col v-else cols="1" class="red--text text-h6 font-weight-bold">
+      <v-col v-else sm="1" xs="12" class="red--text text-h6 font-weight-bold">
         {{ "END" }}
       </v-col>
-      <v-col cols="3" class="font-weight-medium">
-        {{ meeting.start_time }}
+      <v-col sm="3" xs="12" class="font-weight-medium">
+        {{ formatDate(meeting.start_time) }}
       </v-col>
-      <v-col>
+      <v-col sm="8" xs="12">
         {{ meeting.topic }}
       </v-col>
     </v-row>
@@ -27,6 +28,13 @@ export default {
 
   props: ["meeting"],
   data: () => ({}),
+  methods: {
+    formatDate(date) {
+      return new Date(date).toLocaleString("tr-TR", {
+        timeZone: "Turkey",
+      })
+    },
+  },
   created() {
     //console.log(this.meeting)
   },
